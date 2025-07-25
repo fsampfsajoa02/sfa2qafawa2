b57712f2a6ba747a97572609ee656539a1257f08
Kevin <kevin@aspfgnmasmpfas.com>
2025-07-25 02:04:10 +0100

    init

---
+++ inject/r3/r3index.js
@@ -0,0 +1,3 @@
+function copyNote() {
+    navigator.clipboard.writeText(" ( monster ~ tentacle ~ bestiality ~ interspecies ~ ai_generated ~ anthro ~ fur ~ furry ~ slime ~ insects ) -futanari -male_only  -femboy -male/male ");
+}
